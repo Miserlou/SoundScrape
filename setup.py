5889779a8a1bca96e3722ec509f7cebbadceaad5
Jakob Hoeper <JKatzwinkel@users.noreply.github.com>
2020-11-19 11:55:37 +0100

    forgot that travis doesn't support python 3.3

--- setup.py
+++ setup.py
@@ -48,7 +48,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.7',
